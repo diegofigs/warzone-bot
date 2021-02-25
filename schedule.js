6b51d35d664b580b29cb5f6430ef03895118180c
Diego Figueroa <diego@dfigueroa.me>
2021-02-25 18:53:22 -0400

    :construction: (jobs) add daily stats job / scheduling scheme

---
+++ schedule.js
@@ -0,0 +1,12 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+const fs = require('fs');
+const schedule = require('node-schedule');
+
+const jobFiles = fs.readdirSync(`./jobs`).filter(file => file.endsWith('.js'));
+for (const jobName of jobFiles) {
+  const { name, cron, rule, execute } = require(`./jobs/${jobName}`);
+  const frequency = rule ? rule : cron;
+  schedule.scheduleJob(name, frequency, execute);
+}
