8b775ec3d37093521be537917a6f198be5b6bb75
Diego Figueroa <diego@dfigueroa.me>
2021-02-24 20:55:22 -0400

    :construction: (commands/mw) add stats embed for command

--- commands/mw/stats.js
+++ commands/mw/stats.js
@@ -4,14 +4,26 @@ const { getStats } = require('../../core');
 module.exports = {
   name: 'stats',
   aliases: ['highlights'],
-	description: 'Fetch Highest Kills / Most Deaths',
+	description: 'Fetch Highest Kills / Most Deaths of the day',
   args: true,
   usage: '<gamertag> <platform>',
 	execute: async (message, args) => {
     const [gamertag, platform] = args;
     try {
       const { highestKills, mostDeaths } = await getStats({ gamertag, platform });
-      message.channel.send(`Highest Kills: ${highestKills} / Most Deaths: ${mostDeaths}`);
+      const statsEmbed = new Discord.MessageEmbed()
+      .setColor('#0099ff')
+      .setTitle(`${gamertag}'s Stats of the Day`)
+      .setDescription('Brought to you by wz-bot')
+      .setThumbnail(wzImg)
+      .addFields(
+        { name: 'Highest Kills', value: highestKills, inline: true },
+        { name: 'Most Deaths', value: mostDeaths, inline: true },
+      )
+      .setTimestamp()
+      .setFooter('This information is property of Infinity Ward');
+
+      message.channel.send(statsEmbed);
     } catch (e) {
       console.log('error', e);
     }
