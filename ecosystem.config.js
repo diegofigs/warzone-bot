ae8bceefd60126967e20bf853f63fe6fbe7752fe
Diego Figueroa <diego@dfigueroa.me>
2021-02-28 06:21:57 -0400

    :construction:  use different npm binary

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -42,7 +42,10 @@ module.exports = {
       repo : 'git@github.com:diegofigs/warzone-bot.git',
       path : '/root/warzone-bot',
       'pre-deploy-local': '',
-      'post-deploy' : 'nvm use default && npm install && pm2 reload ecosystem.config.js --env production',
+      'post-deploy' : 'source ~/.bashrc && \
+        nvm use default && \
+        npm install && \
+        pm2 reload ecosystem.config.js --env production',
       'pre-setup': ''
     }
   }
