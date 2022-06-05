418619c2a746b7203c3f5b92b3ecaf793f083c2f
Diego Figueroa <12281609+diegofigs@users.noreply.github.com>
2022-06-04 22:30:49 -0400

    refactor: move all source code inside ./src (#21)

--- index.js
+++ index.js
@@ -7,18 +7,18 @@ const Discord = require('discord.js');
 const client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS] });
 client.commands = new Discord.Collection();
 
-const commandFolders = fs.readdirSync('./commands');
+const commandFolders = fs.readdirSync('./src/commands');
 for (const folder of commandFolders) {
-  const commandFiles = fs.readdirSync(`./commands/${folder}`).filter((file) => file.endsWith('.js'));
+  const commandFiles = fs.readdirSync(`./src/commands/${folder}`).filter((file) => file.endsWith('.js'));
   for (const file of commandFiles) {
-    const command = require(`./commands/${folder}/${file}`);
+    const command = require(`./src/commands/${folder}/${file}`);
     client.commands.set(command.name, command);
   }
 }
 
-const eventFiles = fs.readdirSync('./events').filter((file) => file.endsWith('.js'));
+const eventFiles = fs.readdirSync('./src/events').filter((file) => file.endsWith('.js'));
 for (const file of eventFiles) {
-  const event = require(`./events/${file}`);
+  const event = require(`./src/events/${file}`);
   if (event.once) {
     client.once(event.name, (...args) => event.execute(...args, client));
   } else {
