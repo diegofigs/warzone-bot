176204c9f89bd7775e4b15de80999d98996eee82
Diego Figueroa <12281609+diegofigs@users.noreply.github.com>
2022-07-14 03:49:19 +0000

    style: prettify index.js

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ const { Client, Collection, Intents } = require("discord.js");
 
 const client = new Client({
   intents: [Intents.FLAGS.GUILDS],
-  presence: { activity: { name: "with the WZ Bot" } }
+  presence: { activity: { name: "with the WZ Bot" } },
 });
 
 client.commands = new Collection();
 const commandsPath = path.join(__dirname, "src", "commands");
 const commandFiles = fs
   .readdirSync(commandsPath)
-  .filter(file => file.endsWith(".js"));
+  .filter((file) => file.endsWith(".js"));
 
 for (const file of commandFiles) {
   const filePath = path.join(commandsPath, file);
@@ -19,13 +19,13 @@ for (const file of commandFiles) {
   client.commands.set(command.data.name, command);
 }
 
-client.on("ready", client => {
+client.on("ready", (client) => {
   console.info(
     `Logged in as ${client.user.tag} at ${new Date().toLocaleString("en")}`
   );
 });
 
-client.on("interactionCreate", async interaction => {
+client.on("interactionCreate", async (interaction) => {
   if (!interaction.isCommand()) return;
 
   const command = client.commands.get(interaction.commandName);
@@ -38,7 +38,7 @@ client.on("interactionCreate", async interaction => {
     console.error(error);
     await interaction.reply({
       content: "There was an error while executing this command!",
-      ephemeral: true
+      ephemeral: true,
     });
   }
 });
@@ -46,7 +46,7 @@ client.on("interactionCreate", async interaction => {
 // Log our bot in using the token from https://discord.com/developers/applications
 client.login(process.env.DISCORD_TOKEN);
 
-process.on("SIGINT", msg => {
+process.on("SIGINT", (msg) => {
   // process reload ongoing
   // close connections, clear cache, etc
   // by default, you have 1600ms
