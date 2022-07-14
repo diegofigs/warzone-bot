ad2e1a765163921d320bf5c67e661d55e7a3c37f
diegofigs <12281609+diegofigs@users.noreply.github.com>
2022-07-13 23:06:34 -0400

    refactor: remove deprecated events structure

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ const fs = require("node:fs");
 const path = require("node:path");
 const { Client, Collection, Intents } = require("discord.js");
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS],
+  presence: { activity: { name: "with the WZ Bot" } }
+});
 
 client.commands = new Collection();
 const commandsPath = path.join(__dirname, "src", "commands");
 const commandFiles = fs
   .readdirSync(commandsPath)
-  .filter((file) => file.endsWith(".js"));
+  .filter(file => file.endsWith(".js"));
 
 for (const file of commandFiles) {
   const filePath = path.join(commandsPath, file);
@@ -16,19 +19,13 @@ for (const file of commandFiles) {
   client.commands.set(command.data.name, command);
 }
 
-const eventFiles = fs
-  .readdirSync("./src/events")
-  .filter((file) => file.endsWith(".js"));
-for (const file of eventFiles) {
-  const event = require(`./src/events/${file}`);
-  if (event.once) {
-    client.once(event.name, (...args) => event.execute(...args, client));
-  } else {
-    client.on(event.name, (...args) => event.execute(...args, client));
-  }
-}
+client.on("ready", client => {
+  console.info(
+    `Logged in as ${client.user.tag} at ${new Date().toLocaleString("en")}`
+  );
+});
 
-client.on("interactionCreate", async (interaction) => {
+client.on("interactionCreate", async interaction => {
   if (!interaction.isCommand()) return;
 
   const command = client.commands.get(interaction.commandName);
@@ -41,7 +38,7 @@ client.on("interactionCreate", async (interaction) => {
     console.error(error);
     await interaction.reply({
       content: "There was an error while executing this command!",
-      ephemeral: true,
+      ephemeral: true
     });
   }
 });
@@ -49,7 +46,7 @@ client.on("interactionCreate", async (interaction) => {
 // Log our bot in using the token from https://discord.com/developers/applications
 client.login(process.env.DISCORD_TOKEN);
 
-process.on("SIGINT", (msg) => {
+process.on("SIGINT", msg => {
   // process reload ongoing
   // close connections, clear cache, etc
   // by default, you have 1600ms
